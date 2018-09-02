cef9b495b9a521628206c5019781682ff847d4aa
Nicollas Carvalho <n.matheuscarvalho@gmail.com>
2018-09-02 15:34:29 -0300

    first commit

---
+++ src/Components/App.jsx
@@ -0,0 +1,24 @@
+import Re`a`ct, {Component} from 'react'
+import BoaNoite, {BomDia, BoaTarde} from './Day.jsx'
+import Saudation from './Saudation'
+import Father from './Father'
+import Son from './Son'
+
+
+export default class App extends Component {
+    render(){
+        return(
+            <div>
+                <Father nome='Grijalba' sobrenome='Linhares'>
+                    <Son nome='Nicollas' />
+                    <Son nome='Nicollas' sobrenome='Carvlaho' />
+                    <Son nome='Nicollas' sobrenome='Carvlaho' />
+                </Father>
+                <Saudation type={'bomdia'} name={'joao'} />
+                <BomDia nome={'Nicollas'}/>
+                <BoaTarde nome={'Julie'}/>
+                <BoaNoite nome={'Julie'}/>
+            </div>
+        )
+    }
+}
